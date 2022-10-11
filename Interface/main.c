c28946868ac399d32584d4e1573634824aeec6a3
Alexandra Wolter <alexandra.wolter@epita.fr>
2022-10-11 13:28:19 +0200

    interface[interface] changement Makefile

--- Interface/main.c
+++ Interface/main.c
@@ -1,19 +1,23 @@
 #include <gtk/gtk.h>
 
-void interface (GtkApplication app)
+void interface (GtkApplication *app)
 {
-	GtkWidget window = gtk_application_window_new (app);
+	GtkWidget *window = gtk_application_window_new (app);
 	gtk_window_set_title (GTK_WINDOW (window), "Window");
 	gtk_window_set_default_size (GTK_WINDOW (window), 800, 500);
-	GtkWidget button = gtk_button_new_with_label ("Test");
-	gtk_widget_show_all (window);
+	GtkWidget *button = gtk_button_new_with_label ("Test");
+	gtk_widget_show (window);
 }
 
-int main (int argc, char argv[])
+int main (int argc, char **argv)
 {
-	GtkApplication *app = gtk_application_new("com.OCR.app",G_APPLICATION_FLAGS_NONE);
-	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-	status = g_application_run (G_APPLICATION (app), argc, argv);
-	g_object_unref (app);
-	return status;
+	GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "interface", G_CALLBACK (interface), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
 }
