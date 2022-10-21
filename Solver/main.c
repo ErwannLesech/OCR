f77e928479784dab02a52a1b94c250e1d73f92f5
guillaume.jolivalt <guillaume.jolivalt@epita.fr>
2022-10-22 00:50:48 +0200

    solver<main.c> reading and writing in file finished

--- Solver/main.c
+++ Solver/main.c
@@ -3,39 +3,52 @@
 #include <err.h>
 #include "solver.h"
 
-void print_sudoku(char argv[9][9]);
 char a[9][9] = 
 {
-	{'5','3','.','.','7','.','.','.','.'},
-	{'6','.','.','1','9','5','.','.','.'},
-	{'.','9','8','.','.','.','.','6','.'},
-	{'8','.','.','.','6','.','.','.','3'},
-	{'4','.','.','8','.','3','.','.','1'},
-	{'7','.','.','.','2','.','.','.','6'},
-	{'.','6','.','.','.','.','2','8','.'},
-	{'.','.','.','4','1','9','.','.','5'},
-	{'.','.','.','.','8','.','.','7','9'}
+	{'.','.','.','.','.','.','.','.','.'},
+	{'.','.','.','.','.','.','.','.','.'},
+	{'.','.','.','.','.','.','.','.','.'},
+	{'.','.','.','.','.','.','.','.','.'},
+	{'.','.','.','.','.','.','.','.','.'},
+	{'.','.','.','.','.','.','.','.','.'},
+	{'.','.','.','.','.','.','.','.','.'},
+	{'.','.','.','.','.','.','.','.','.'},
+	{'.','.','.','.','.','.','.','.','.'}
 };
 
-int main(int argc, char** argv[9][9])
+
+void read_sudoku(char *filename)
 {
-	/*if(argc != 2)
+	FILE *input_file = fopen(filename, "r");
+	int i = 0;
+	int j = 0;
+	char c;
+	
+	while((c=fgetc(input_file)) != EOF)
 	{
-		errx(EXIT_FAILURE, "%s", "A sudoku is needed");
+		if(c != ' ' && c != '\n')
+		{
+			a[i][j] = c;
+			j++;
+
+			if(j > 8)
+			{
+				j = 0;
+				i++;
+			}
+		}
+	}
+	
+	/*char *content;
+	while(fscanf(input_file, "%[n]", content) != EOF)
+	{
+		printf("> %s\n", content);
 	}*/
-
-	//else
-	//{
-		print_sudoku(a);
-		solver(a);
-		print_sudoku(a);
-	//}
-
-	return EXIT_SUCCESS;
+	fclose(input_file);
 }
 
 
-void print_sudoku(char argv[9][9])
+void print_sudoku()
 {
 	for(unsigned int i = 0; i < 9; i++)
 	{
@@ -51,7 +64,7 @@ void print_sudoku(char argv[9][9])
 				printf(" ");
 			}
 			
-			printf("%c", argv[i][j]);
+			printf("%c", a[i][j]);
 		}
 
 		printf("\n");
@@ -59,4 +72,68 @@ void print_sudoku(char argv[9][9])
 	}
 
 	printf("\n");
+}
+
+
+void save_sudoku(char *filename)
+{
+	FILE* output_file = fopen(filename, "w");
+
+	for(unsigned int i = 0; i < 9; i++)
+	{
+		if(i%3 == 0 && i != 0)
+                {
+			//fwrite('\n', 1, 1, output_file);
+			//write(output_file, "space", 5);
+			fprintf(output_file, "\n");
+                }
+
+		for(unsigned int j = 0; j < 9; j++)
+		{
+			if(j%3 == 0 && j != 0)
+			{
+				//fwrite(" ", 1, 1, output_file);
+				//write(output_file, " ", 1);
+				fprintf(output_file, " ");
+			}
+			
+			//fwrite(a[i][j], 1, 1, output_file);
+			//write(output_file, a[i][j], 1);
+			/*printf("%c", a[i][j]);
+			printf("%i", i);
+			printf("%i\n", j);*/
+
+			fprintf(output_file, "%c", a[i][j]);
+		}
+
+		//fwrite('\n', 1, 1, output_file);
+		//write(output_file, "space", 5);
+		fprintf(output_file, "\n");
+	}
+
+	//fwrite('\n', 1, 1, output_file);
+	//write(output_file, "space", 5);
+	fprintf(output_file, "\n");
+	fclose(output_file);
+}
+
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		errx(EXIT_FAILURE, "%s", "A valid sudoku is needed");
+	}
+
+	else
+	{
+		read_sudoku(argv[1]);
+		printf("a");
+		solver(a);
+		printf("b");
+		char *filename = "test_grid.result";
+		save_sudoku(filename);
+	}
+
+	return EXIT_SUCCESS;
 }
