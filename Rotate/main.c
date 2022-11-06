d3a57caef44653baf330d3784c8b9a4420ab5c9e
alexandra.wolter <alexandra.wolter@epita.fr>
2022-11-06 10:22:21 +0100

    comments of rotate

--- Rotate/main.c
+++ Rotate/main.c
@@ -6,72 +6,93 @@
 #include <SDL2/SDL_image.h>
 #include <math.h>
 
-SDL_Surface* rotate_img(SDL_Surface* sdl, double degree){
-	SDL_LockSurface(sdl);
-	 SDL_Surface* new_surface = SDL_ConvertSurfaceFormat(sdl, 
+SDL_Surface* rotate_img(SDL_Surface* surface, double degree)
+{
+	//Lock the surface
+	SDL_LockSurface(surface);
+
+	//Create a new surface in SDL_PIXELFORMAT_RGB888
+	SDL_Surface* new_surface = SDL_ConvertSurfaceFormat(surface, 
 	 	SDL_PIXELFORMAT_RGB888, 0);
-    SDL_FreeSurface(sdl);
-	SDL_Surface* sdl_copy = SDL_CreateRGBSurface(0,sdl->w,sdl->h,
+
+    	SDL_FreeSurface(surface);
+	
+	//Create a copy of the surface
+	SDL_Surface* surface_copy = SDL_CreateRGBSurface(0,surface->w,surface->h,
 		32,0,0,0,0);
-	SDL_LockSurface(sdl_copy);
+
+	//Lock the surfaces
+	SDL_LockSurface(surface_copy);
 	SDL_LockSurface(new_surface);
-	Uint32* pixels_copy = sdl_copy->pixels;
-	Uint32* pixels = new_surface->pixels; 
+
+	//Get the pixels
+	Uint32* pixels_copy = surface_copy->pixels;
+	Uint32* pixels = new_surface->pixels;
+
+	//Convert the angle in degree to angle in radian
 	double angle_rad = degree* M_PI/ 180.0;
-	double center_x = (sdl->w/(double)2);
-	double center_y = (sdl->h/(double)2);
-	for (int x = 0; x < sdl->w; x++)
+
+
+	double center_x = (surface->w/(double)2);
+	double center_y = (surface->h/(double)2);
+
+	for (int x = 0; x < surface->w; x++)
 	{
-		for (int y = 0; y < sdl->h; y++)
+		for (int y = 0; y < surface->h; y++)
 		{
-			double rx = (x - center_x) * cos(angle_rad) - (y - center_y) * 
-				sin(angle_rad) + center_x;
-			double ry =  (x - center_x) * sin(angle_rad) + (y - center_y) * 
-				cos(angle_rad) + center_y;
+			double rx = (x - center_x) * cos(angle_rad) -
+			       	(y - center_y) * sin(angle_rad) + center_x;
+
+			double ry =  (x - center_x) * sin(angle_rad) + 
+				(y - center_y) * cos(angle_rad) + center_y;
+
 			if (0 <= rx && rx < new_surface->w && 0 <= ry && 
 				ry < new_surface->h)
 			{
-				  Uint8 r,g,b;
-           		SDL_GetRGB(pixels[((int)ry)*(new_surface->w) + (int)rx], 
-					new_surface->format, &r, &g,&b);
-				pixels_copy[((int)y)*(new_surface->w) + (int)x] = 
-					SDL_MapRGB(new_surface->format, r,g,b);
-				//pixels[sdl->pixels[y*w +x]];
+				Uint8 r,g,b;
+           			SDL_GetRGB(pixels[((int)ry)*(new_surface->w) +
+				(int)rx], new_surface->format, &r, &g,&b);
+
+				pixels_copy[((int)y)*(new_surface->w) + (int)x] 
+				= SDL_MapRGB(new_surface->format, r,g,b);
+			
 			}
-			else{
-				pixels_copy[((int)y)*(new_surface->w) + (int)x] = 
-					SDL_MapRGB(new_surface->format, 0,0,0);
+			else
+			{
+				pixels_copy[((int)y)*(new_surface->w) + (int)x] 
+				= SDL_MapRGB(new_surface->format, 0,0,0);
 			}
 			
 		}
 		
 	}
-	SDL_UnlockSurface(sdl_copy);
-	SDL_UnlockSurface(sdl);
-	return sdl_copy;
+	SDL_UnlockSurface(surface_copy);
+	SDL_UnlockSurface(surface);
+	return surface_copy;
 }
 
 
 int main_rotate(int argc, char** argv)
 {
+	//Check the number of arguments
 	if (argc != 4)
         	errx(EXIT_FAILURE, "Not enougth arguments");
-
+	
+	//Init the SDL
 	SDL_Init(SDL_INIT_VIDEO);
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 		errx(EXIT_FAILURE, "%s", SDL_GetError());
-
+	
+	//Create the SDL_Surface from a .jpeg
 	SDL_Surface* surface= IMG_Load(argv[2]);
-
+	
+	//Convert the angle given in long int
 	long int angler = strtol(argv[3], NULL, 10);
 	
-	/*SDL_Surface* turned;
+	//Rotation of the surface
+	SDL_Surface* turned = rotate_img(surface, -angler);
 
-	turned = surface;//rotozoomSurface(surface, -angler, 1,10);
-	*/
-	SDL_Surface* turned = rotate_img(surface, angler);
-
-
+	//Save of the rotation
 	SDL_SaveBMP(turned, "test2.bmp");
 	
 	SDL_FreeSurface(surface);
