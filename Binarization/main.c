0476a073105e844e2a6db5e17166f5bbc75d75c5
guillaume.jolivalt <guillaume.jolivalt@epita.fr>
2022-11-24 16:55:03 +0100

    bin<ocr>: median filter not enough efficient

--- Binarization/main.c
+++ Binarization/main.c
@@ -3,6 +3,84 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+SDL_Surface* init_img(char* path)
+{
+    SDL_Surface* surface = IMG_Load(path);
+    SDL_Surface* new_surface = SDL_ConvertSurfaceFormat(surface, 
+        SDL_PIXELFORMAT_RGB888, 0);
+    SDL_FreeSurface(surface);
+    return new_surface;
+}
+
+
+void draw(SDL_Renderer* renderer, SDL_Texture* texture)
+{
+    SDL_RenderCopy(renderer,texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+}
+
+
+void event_loop(SDL_Renderer* renderer, SDL_Texture* colored)
+{
+    SDL_Event event;
+    draw(renderer, colored);
+    while (1)
+    {
+        SDL_WaitEvent(&event);
+
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                return;
+            case SDL_WINDOWEVENT:
+             if (event.window.event == SDL_WINDOWEVENT_RESIZED)
+                {
+                draw(renderer, colored);
+                }
+                break;
+        }
+    }
+}
+
+
+void display_img(SDL_Surface* sdl_surface)
+{
+   if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    
+   // - Create a window.
+    SDL_Window* window = SDL_CreateWindow("Display", 0, 0, 640, 400,
+            SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (window == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    
+    // - Create a renderer.
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 
+            SDL_RENDERER_TARGETTEXTURE);
+    if (renderer == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+       
+    // - Resize the window according to the size of the image.
+    SDL_SetWindowSize(window, sdl_surface->w, sdl_surface->h);
+    
+    // - Create a texture from the colored surface.
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, sdl_surface);
+     
+    // - Free the surface.
+    SDL_FreeSurface(sdl_surface);
+    
+    // - Dispatch the events.
+    event_loop(renderer, texture);
+    
+    // - Destroy the objects.
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+
+    //return EXIT_SUCCESS;
+}
 
 int main(int argc, char **argv)
 {
@@ -13,7 +91,13 @@ int main(int argc, char **argv)
 	{
 		printf("%i, ",arr[i]);
 	}*/
+	
+	SDL_Surface *image = init_img(argv[1]);
+	
+	median_filter(image);
+	//display_img(image);
 
-	median_filter(argv[1]);
+	SDL_SaveBMP(image, "image_median.bmp");
+	free(image);
 	return EXIT_SUCCESS;
 }
--- Binarization/otsu.c
+++ Binarization/otsu.c
@@ -169,7 +169,7 @@ Uint32 pixel_to_grayscale(Uint32 pixel_color, SDL_PixelFormat* format)
 {
     Uint8 r, g, b;
     SDL_GetRGB(pixel_color, format, &r, &g, &b);
-    Uint8 average = 0.3*r + 0.59*g+0.11*b;
+    Uint8 average = 0.3*r + 0.59*g + 0.11*b;
     r = g = b = average;
     Uint32 color = SDL_MapRGB(format, r, g, b);
     return color;
@@ -201,6 +201,7 @@ void print_array(int **array, int x, int y)
 	{
 		for(int j = 0; j < y; j++)
 		{
+			//printf("%i ", array[i * y + j]);
 			printf("%i ", array[i][j]);
 		}
 
@@ -212,28 +213,41 @@ void median_filter(SDL_Surface *image)
 {
 	const unsigned int height = image->h;
 	const unsigned int width = image->w;
-	int **array = malloc(height * width * sizeof(int*));
-	int **arr = malloc(height * width * sizeof(int*));
-	//int array[height][width];
-	//int arr[height][width];
+	int **array = malloc(height * sizeof(int*));
+	int **arr = malloc(height * sizeof(int*));
+	//int array[height * width];
+	//int arr[height * width];
+	//print_array(array, height, width);
 	int temp[9];
 	Uint32 *pixels = image->pixels;
-	SDL_Surface *surface = IMG_Load(image);
-	SDL_PixelFormat* format = surface->format;
+	SDL_PixelFormat* format = image->format;
+
+	for(int i = 0; i < height; i++)
+	{
+		array[i] = (int*)malloc(width * sizeof(int));
+		arr[i] = (int*)malloc(width * sizeof(int));
+	}
+	
+	//free(array);
+	//free(arr);
 
 	for(int row = 0; row < height; row++)
 	{
-		array[row][0] = pixel_to_grayscale(pixels[row*height], format);
-		arr[row][0] = 0;
+		for(int col = 0; col < width; col++)
+		{
+			//printf("%i\n", pixel_to_grayscale(pixels[row * width + col], format));
+			array[row][col] = pixel_to_grayscale(pixels[row * width + col], format);
+			arr[row][col] = 0;
+		}
 	}
 
-	for(int col = 0; col < width; col++)
+	/*for(int col = 0; col < width; col++)
 	{
 		array[0][col] = pixel_to_grayscale(pixels[col*height], format);
 		arr[0][col] = 0;
-	}
+	}*/
 
-	print_array(array, height, width);
+	//print_array(array, height, width);
 
 	for(int row = 1; row < height-1; row++)
 	{
@@ -253,6 +267,17 @@ void median_filter(SDL_Surface *image)
             		arr[row][col] = temp[4];
 		}
 	}
+	
+	for(int a = 0; a < height; a++)
+	{
+		for(int b = 0; b < width; b++)
+		{
+			pixels[a * width + b] = arr[a][b];
+		}
+	}
 
-	print_array(array, height, width);
+	//print_array(array, height, width);
+	//print_array(arr, height, width);
+	free(array);
+	free(arr);
 }
