0a0930054b70a0ef7142d12df172ea1e60b22a2d
Alexandra Wolter <alexandra.wolter@epita.fr>
2022-11-30 07:58:36 +0100

    suite interface

--- Interface/interface.c
+++ Interface/interface.c
@@ -7,10 +7,13 @@ static GtkWidget* fixed;
 static GtkWidget* fileChooser;
 static GtkWidget* button1;
 static GtkWidget* button2;
-static GtkWidget* label;
+static GtkWidget* button3;
+static GtkWidget* label1;
+static GtkWidget* label2;
 static GtkWidget* preview;
 static char* file;
 static int bsolve;
+static int stepc;
 
 int valid_format(int len)
 {
@@ -32,11 +35,11 @@ void updateImage(GtkFileChooser *fc)
     {
         if(valid_format(strlen(file)))
 	{
-            gtk_label_set_text(GTK_LABEL(label), "THE FORMAT IS VALID !");
+            gtk_label_set_text(GTK_LABEL(label1), "THE FORMAT IS VALID !");
         }
        	else 
 	{
-            gtk_label_set_text(GTK_LABEL(label), "Please choose a .pneg or .jpg");
+            gtk_label_set_text(GTK_LABEL(label1), "Please choose a .pneg or .jpg");
         }
 	
         gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*) file);
@@ -53,40 +56,45 @@ void quickOCR()
             switch(bsolve) 
 	    {
             case 0:
-                gtk_label_set_text(GTK_LABEL(label), "Choose Your Grid !");
+                gtk_label_set_text(GTK_LABEL(label1), "Choose Your Grid !");
                 break;
             case 1:
-                gtk_label_set_text(GTK_LABEL(label), "CHOOSE YOUR GRID !");
+                gtk_label_set_text(GTK_LABEL(label1), "CHOOSE YOUR GRID !");
                 break;
             case 2:
-                gtk_label_set_text(GTK_LABEL(label), "CHOOSE IT !!!!!!");
+                gtk_label_set_text(GTK_LABEL(label1), "CHOOSE IT !!!!!!");
+		gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*) "error_image1.jpg");
                 break;
             }
             bsolve += bsolve <= 2;
         }
-        /*else if(lenF > 4) {
-            if(valid_format(lenF)) 
-	    {
+        /*else if(lenF > 4) 
+	 {
+            	if(valid_format(lenF)) 
+	    	{
                 
-                gtk_label_set_text(GTK_LABEL(label), "Processing...");
-                if(main_ImageProcess(file))
-		{
-                    gtk_label_set_text(GTK_LABEL(label), "Tadaaaaaa!");
-                    gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
-                        "results/step9_Solved.png");
-                } 
-		else 
-		{
-                    gtk_label_set_text(GTK_LABEL(label), "No Solution Found !");
-                }
-            } 
-	    else 
-	    {
-                gtk_label_set_text(GTK_LABEL(label), ".PNG OR .JPG");
-            }
+                	gtk_label_set_text(GTK_LABEL(label), "Processing...");
+                	if(main_ImageProcess(file))
+			{
+                    		gtk_label_set_text(GTK_LABEL(label), "Tadaaaaaa!");
+                    		gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        	"results/step9_Solved.png");
+                	}
+			else 
+			{
+                    		gtk_label_set_text(GTK_LABEL(label), "No Solution Found !");
+				gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+				"error_image2.png");
+                	}
+            	} 
+	    	else 
+	    	{
+                	gtk_label_set_text(GTK_LABEL(label), ".PNG OR .JPG");
+            	}
         }*/
     }
 }
+
 void step_click_OCR()
 {
 	if(file)
@@ -97,13 +105,14 @@ void step_click_OCR()
             		switch(bsolve) 
 	    		{
             		case 0:
-                		gtk_label_set_text(GTK_LABEL(label), "Choose Your Grid !");
+                		gtk_label_set_text(GTK_LABEL(label1), "Choose Your Grid !");
                 		break;
             		case 1:
-                		gtk_label_set_text(GTK_LABEL(label), "CHOOSE YOUR GRID !");
+                		gtk_label_set_text(GTK_LABEL(label1), "CHOOSE YOUR GRID !");
                 		break;
             		case 2:
-                		gtk_label_set_text(GTK_LABEL(label), "CHOOSE IT !!!!!!");
+                		gtk_label_set_text(GTK_LABEL(label1), "CHOOSE IT !!!!!!");
+				gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*) "error_image1.jpg");
                 		break;
             		}
            	 	bsolve += bsolve <= 2;
@@ -111,40 +120,63 @@ void step_click_OCR()
 	}
 	/*else* if(lenF >4)
 	{
-		gtk_label_set_text(GTK_LABEL(label), "Processing ...");
+		gtk_label_set_text(GTK_LABEL(label1), "Let's gooooooo");
 		if(main_ImageProcess(file))
 		{
-			gtk_label_set_text(GTK_LABEL(label), "Grayscale");
-                    	gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
-                        "results/step9_Solved.png");
+			switch(stepc)
+			{
+			case 0:
+				gtk_label_set_text(GTK_LABEL(label2), "Grayscale");
+                    		gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        	"results/step9_Solved.png");
+				break;
+			case 1:
+				gtk_label_set_text(GTK_LABEL(label2), "Binarization -->");
+				gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        	"results/step9_Solved.png");
+				break;
+			case 2:
+				gtk_label_set_text(GTK_LABEL(label2), "Sobel Filter -->");
+				gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        	"results/step9_Solved.png");
+				break;
+			case 3:
+				gtk_label_set_text(GTK_LABEL(label2), "Hough Transform -->");
+				gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        	"results/step9_Solved.png");
+				break;
+			case 4:
+				gtk_label_set_text(GTK_LABEL(label2), "Separate -->");
+				gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        	"results/step9_Solved.png");
+				break;
+			case 5:
+				gtk_label_set_text(GTK_LABEL(label2), "Processing -->");
 
-			gtk_label_set_text(GTK_LABEL(label), "Binarization");
-
-			gtk_label_set_text(GTK_LABEL(label), "Sobel Filter");
-
-			gtk_label_set_text(GTK_LABEL(label), "Hough Transform")
-
-			gtk_label_set_text(GTK_LABEL(label), "Separate")
-
-			gtk_label_set_text(GTK_LABEL(label), "Processing ...")
-
-
-
-
-                } 
-		else 
+				gtk_label_set_text(GTK_LABEL(label2), "Solution ! -->");
+				gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        	"results/step9_Solved.png");
+				gtk_label_set_text(GTK_LABEL(label1), "We did it !");
+			}
+			stepc += stepc <= 5;
+		}
+		else
 		{
-                    gtk_label_set_text(GTK_LABEL(label), "No Solution Found !");
+			gtk_label_set_text(GTK_lABEL(label1), "Oh nooooo :'(")
+                    	gtk_label_set_text(GTK_LABEL(label2), "No Solution Found !");
+			gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+			"error_image2.png");
                 }
             } 
-	    else 
+	    else
 	    {
-                gtk_label_set_text(GTK_LABEL(label), ".PNG OR .JPG");
+                gtk_label_set_text(GTK_LABEL(label), ".png OR .jpeg please");
             }
         }
 	 */
 }
-void stepOCR()
+
+void step_OCR()
 {
 	if(file)
     	{
@@ -154,18 +186,76 @@ void stepOCR()
             		switch(bsolve) 
 	    		{
             		case 0:
-                		gtk_label_set_text(GTK_LABEL(label), "Choose Your Grid !");
+                		gtk_label_set_text(GTK_LABEL(label1), "Choose Your Grid !");
                 		break;
             		case 1:
-                		gtk_label_set_text(GTK_LABEL(label), "CHOOSE YOUR GRID !");
+                		gtk_label_set_text(GTK_LABEL(label1), "CHOOSE YOUR GRID !");
                 		break;
             		case 2:
-                		gtk_label_set_text(GTK_LABEL(label), "CHOOSE IT !!!!!!");
+                		gtk_label_set_text(GTK_LABEL(label1), "CHOOSE IT !!!!!!");
+				gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*) "error_image1.jpg");
                 		break;
             		}
            	 	bsolve += bsolve <= 2;
         	}
 	}
+		/*else* if(lenF >4)
+		{
+			gtk_label_set_text(GTK_LABEL(label1), "Let's gooooooo");
+			if(main_ImageProcess(file))
+			{
+				switch(stepc)
+				{
+				case 0:
+					gtk_label_set_text(GTK_LABEL(label2), "Grayscale");
+                    			gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        		"results/step9_Solved.png");
+					break;
+				case 1:
+					gtk_label_set_text(GTK_LABEL(label2), "Binarization -->");
+					gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+        	                	"results/step9_Solved.png");
+					break;
+				case 2:
+					gtk_label_set_text(GTK_LABEL(label2), "Sobel Filter -->");
+					gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        		"results/step9_Solved.png");
+					break;
+				case 3:
+					gtk_label_set_text(GTK_LABEL(label2), "Hough Transform -->");
+					gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        	"results/step9_Solved.png");
+					break;
+				case 4:
+					gtk_label_set_text(GTK_LABEL(label2), "Separate -->");
+					gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        	"results/step9_Solved.png");
+					break;
+				case 5:
+					gtk_label_set_text(GTK_LABEL(label2), "Processing -->");
+
+					gtk_label_set_text(GTK_LABEL(label2), "Solution ! -->");
+					gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+                        		"results/step9_Solved.png");
+					gtk_label_set_text(GTK_LABEL(label1), "We did it !");
+                
+				}
+				stepc += stepc <= 5;
+			}
+			else
+			{
+				gtk_label_set_text(GTK_lABEL(label1), "Oh nooooo :'(")
+                    		gtk_label_set_text(GTK_LABEL(label2), "No Solution Found !");
+				gtk_image_set_from_file(GTK_IMAGE(preview), (const gchar*)
+				"error_image2.png");
+			}
+                }
+	}
+	else
+	{
+        	gtk_label_set_text(GTK_LABEL(label), ".png OR .jpeg please");
+        }
+*/
 }
 
 int main/*_interface*/ (int argc, char **argv)
@@ -173,6 +263,7 @@ int main/*_interface*/ (int argc, char **argv)
 	gtk_init(&argc, &argv);
     	file = "";
     	bsolve = 0;
+	stepc = 0;
 
     	builder = gtk_builder_new_from_file("interface.glade");
 	window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
@@ -180,9 +271,10 @@ int main/*_interface*/ (int argc, char **argv)
     	fileChooser = GTK_WIDGET(gtk_builder_get_object(builder, "fileChooser"));
     	button1 = GTK_WIDGET(gtk_builder_get_object(builder, "button1"));
 	button2 = GTK_WIDGET(gtk_builder_get_object(builder, "button2"));
-	label = GTK_WIDGET(gtk_builder_get_object(builder, "label"));
+	button3 = GTK_WIDGET(gtk_builder_get_object(builder, "button3"));
+	label1 = GTK_WIDGET(gtk_builder_get_object(builder, "label1"));
+	label2 = GTK_WIDGET(gtk_builder_get_object(builder, "label2"));
     	preview = GTK_WIDGET(gtk_builder_get_object(builder, "preview"));
-
 
     	g_signal_connect(window, "destroy",
         	G_CALLBACK(gtk_main_quit), NULL);
@@ -191,7 +283,9 @@ int main/*_interface*/ (int argc, char **argv)
     	g_signal_connect(GTK_BUTTON(button1), "clicked",
         	G_CALLBACK(quickOCR), NULL);
 	g_signal_connect(GTK_BUTTON(button2),"clicked",
-		G_CALLBACK(stepOCR),NULL);
+		G_CALLBACK(step_OCR),NULL);
+	g_signal_connect(GTK_BUTTON(button3),"clicked",
+		G_CALLBACK(step_click_OCR), NULL);
 
     gtk_widget_show(window);
     gtk_main();
